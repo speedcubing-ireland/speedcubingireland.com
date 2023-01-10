451cff63ccf2e980ee7612e22c8691f417af89ee
Simon Kelly <simonkellly@gmail.com>
2023-01-10 00:00:03 +0000

    Add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Welcome to Speedcubing Ireland</h1>
+        <h1 className="text-3xl font-bold underline">
+          Welcome to Speedcubing Ireland
+        </h1>
       </main>
     </>
   );
