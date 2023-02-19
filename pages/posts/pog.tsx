b81e908562ae4a45d013d7b1b79c49017c1148b4
Simon Kelly <simonkellly@gmail.com>
2023-02-19 15:25:47 +0000

    Delete unused test file

--- pages/posts/pog.tsx
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { faDiscord, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Link from 'next/link';
-import Post from '../../components/posts/Post';
