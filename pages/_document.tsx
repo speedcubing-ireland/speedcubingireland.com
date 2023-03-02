859c3dad27a9e745acd9b5abbb609c3601052b43
Simon Kelly <simonkellly@gmail.com>
2023-03-02 19:26:13 +0000

    Remove viewport tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,6 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="index, follow" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
