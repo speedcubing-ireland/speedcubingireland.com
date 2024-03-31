4b17ec75ddac9e0e051c1530e9b3f38b1553014b
Simon Kelly <simonkellly@gmail.com>
2024-03-31 18:06:13 +0100

    Update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,md,mdx}",
   ],
   theme: {
     extend: {},
