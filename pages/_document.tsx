65d362fd27320d0e824941f16d2f44ad6fbfb749
Simon Kelly <simonkellly@gmail.com>
2025-06-14 03:12:21 +0100

    undo accidental changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,22 +9,6 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="robots" content="index, follow" />
-        
-        {/* Font preloading for Safari performance */}
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
