aa98b895b64141921f6958df8ee3e91c3a49716a
Fred Laurent <dreft.laurent@gmail.com>
2026-01-21 22:31:26 +0000

    Update stats

--- components/home/Stats.tsx
+++ components/home/Stats.tsx
@@ -19,13 +19,13 @@ function Stat(title: string, value: string, color: string, icon: any) {
 function Stats() {
   const stats = [
     {
-      title: 'Competitors', value: '1,150+', color: 'text-secondary', icon: faPeopleGroup,
+      title: 'Competitors', value: '1,750+', color: 'text-secondary', icon: faPeopleGroup,
     },
     {
-      title: 'Solves', value: '110,000+', color: 'text-primary', icon: faStopwatch,
+      title: 'Solves', value: '270,000+', color: 'text-primary', icon: faStopwatch,
     },
     {
-      title: 'Competitions', value: '50+', color: 'text-secondary', icon: faTrophy,
+      title: 'Competitions', value: '85+', color: 'text-secondary', icon: faTrophy,
     },
   ];
 
