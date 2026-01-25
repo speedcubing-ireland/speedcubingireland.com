d9c1c31a6e7694e98b0623f2206902ad1ca4274e
Fred Laurent <dreft.laurent@gmail.com>
2026-01-25 20:52:19 +0000

    Handle env vars as part of test GH action

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,16 @@ export default defineConfig({
     environment: 'node',
     include: ['**/*.test.ts'],
     exclude: ['node_modules', '.next'],
+    env: {
+      SHOPIFY_API_KEY: 'getfromshopify',
+      SHOPIFY_API_SECRET_KEY: 'getfromshopify',
+      SHOPIFY_ADMIN_API_KEY: 'getfromshopify',
+      SHOPIFY_HOSTNAME: 'yourstorename.myshopify.com',
+      SHOPIFY_STORE: 'specialdefaultstorename',
+      REVALIDATE_SECRET: 'thisisrandomstringthatyoumakeup',
+      EUROS_CODE: 'codeforeurosorothermerch',
+      KIDS_LINK: 'link',
+      ADULTS_LINK: 'link',
+    },
   },
 });
